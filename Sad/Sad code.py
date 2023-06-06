fe18fd8bac2bf6ceb5a6bfab321f49937b55759b
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-06-06 18:18:45 +0530

    Kind of Chaos Theory Application 😃- A simple change in the initial conditons can lead to cataclysmic effect.

---
+++ Sad/Sad code.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 2*np.pi, 1000)
+x_face = 4 * np.sin(t)
+y_face = 4 * np.cos(t)
+
+x_mouth = np.linspace(-1.5, 1.5, 100)
+y_mouth = 0.8 * np.cos(x_mouth) - 2 # This part is very interesting. In case, of the Smiley face, we had a (-) minus sign before 0.8, and here it's positive. So a simple change in the equation is changing the whole plot.
+
+plt.fill(x_face, y_face, color='yellow')
+plt.plot(x_mouth, y_mouth, color='black', linewidth=3)
+
+# Add circular black eyes
+
+eye_radius = 16
+eye_y = 1.8
+
+plt.scatter(-1.2, eye_y, color='black', s=100)  # Left eye
+plt.scatter(1.2, eye_y, color='black', s=100)   # Right eye
+
+plt.axis('equal')
+plt.axis('off')
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
+
+plt.show()
