95995ec9c44d8391abaf6a1d0e24cc7abc849784
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-06-06 11:47:00 +0530

    Update  ❤️

--- Heart.py
+++ Heart.py
@@ -12,9 +12,9 @@ y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2 * np.cos(3*t) - np.cos(4*t)
 # Creating the plot
 
 plt.figure(figsize=(8, 8))
-plt.plot(x, y, color='red', linewidth=2)
-plt.fill(x, y, 'red')
-
+plt.plot(x, y, color='red', linewidth=2)  # Red Color
+plt.fill(x, y, 'red')                     # Filling the Curve with Red
+ 
 # Plot properties
 
 plt.axis('off')
